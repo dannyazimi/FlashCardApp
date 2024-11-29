4670b799eeb2065d0eaa2c6874d61870661db605
Danny Azimi <k6m3lo@gmail.com>
2024-11-28 17:43:06 -0800

    The program outputs a csv file what_to_learn for all the cards user has not memorized yet

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import random
 import time
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card={}
+to_learn={}
+try:
+    data = pandas.read_csv("data/wat_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 def next_card():
     global current_card, flip_timer
@@ -24,6 +29,12 @@ def flip_card():
     canvas.itemconfig(card_word, text= current_card["English"],fill="white")
     canvas.itemconfig(card_background, image=card_back_image)
 
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/what_to_learn.csv")
+    next_card()
+
 window= Tk()
 window.title("Flash Card")
 window.config(bg = BACKGROUND_COLOR, padx = 50, pady = 50)
@@ -36,10 +47,10 @@ card_front_image = PhotoImage(file = "images/card_front.png")
 card_back_image = PhotoImage(file = "images/card_back.png")
 
 #The buttons
-right_button = Button(image=right_image, highlightthickness=0,command=next_card)
-right_button.grid(row=1, column =1)
-wrong_Button = Button(image=wrong_image, highlightthickness=0,command=next_card)
-wrong_Button.grid(row=1, column =0)
+know_it = Button(image=right_image, highlightthickness=0,command=is_known)
+know_it.grid(row=1, column =1)
+dont_know_it = Button(image=wrong_image, highlightthickness=0,command=next_card)
+dont_know_it.grid(row=1, column =0)
 
 #The middle card as canvas
 canvas = Canvas(width = 800, height = 526, bg=BACKGROUND_COLOR, highlightthickness=0)
